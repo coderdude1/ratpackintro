5e1e46b068a60f1876681e79c296449c21b847f7
Randy <drbugsmn@gmail.com>
2019-05-15 14:06:05 -0500

    Finished chapter 5 examples on handlers

--- src/main/java/com/dood/ratpack/intro/ch5/RouterChain.java
+++ src/main/java/com/dood/ratpack/intro/ch5/RouterChain.java
@@ -4,6 +4,9 @@ import ratpack.func.Action;
 import ratpack.handling.Chain;
 import ratpack.handling.Handler;
 
+/**
+ * This does the same thing as the Router class, just with less code.
+ */
 public class RouterChain implements Action<Chain> {
     private final Handler fooHandler = new FooHandler();
     private final Handler barHandler = new BarHandler();
